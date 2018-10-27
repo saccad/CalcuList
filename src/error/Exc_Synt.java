3c6e6c99726f78504bd0e0436213d07b7a0c8775
saccad <sacca@unical.it>
2018-10-27 17:09:09 +0200

    Added GPL v3.0 preamble
    - added the GPL v3.0 preamble in each classe

--- src/error/Exc_Synt.java
+++ src/error/Exc_Synt.java
@@ -1,3 +1,25 @@
+/**********************************************************************
+CalcuList (Calculator with List manipulation) is an educational 
+language for teaching functional programming extended with some 
+imperative and side-effect features, which are enabled under explicit 
+request by the programmer.
+
+Copyright (C) 2018  Prof. Saccà Domenico - University of Calabria (Italy)
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+**********************************************************************/
+
 package error;
 
 
